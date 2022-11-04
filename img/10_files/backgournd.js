b5e5a5840173dfaa37c303ee773bd9c29a37737f
frank <syncstar9@gmail.com>
2022-11-05 00:19:30 +0900

    그림판추가

--- img/10_files/backgournd.js
+++
@@ -1,6 +0,0 @@
-const images = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg"];
-
-const body = document.querySelector("body");
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-body.style.backgroundImage = `url('img/bg/${chosenImage}')`;
